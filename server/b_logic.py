02f26263c2d489942822544397b752dd42c24c68
nico <nvidoni@ethz.ch>
2020-11-07 17:35:14 +0100

    list comprehension

--- server/b_logic.py
+++ server/b_logic.py
@@ -5,32 +5,29 @@ import database as db
 def all_mock_ops():
     
     """
-    starting point for you
-    call the db.get(mock_ops, [lsit of columns (specify from ID, abbreviation, name, lat, long, didok_id)], optional=Filters
-    
-    Basic function for now                           
+    Fallback function for returning all the mock ops
     """
     
     #list of data that we receive from front end
     #ldat_received = ['ID', 'abbreviation', 'name', 'lat', 'long', 'didok_id']
-    ldat_received = ['name', 'lat', 'long']
+    ldat_received = ('name', 'lat', 'long')
     
     
     # get the data
     #db_out = (db.get(mock_ops, ldat_received, filters = None))
-    db_out = (db.get(mock_ops, ldat_received))
-    
+    rows = db.get(mock_ops, ldat_received)
+    return [{'name':row[0], 'lat':row[1], 'long':row[2]} for row in rows]
     
     # convert the data 
-    ret_list = []
+    # ret_list = []
     
-    for element in db_out:
-        ret_list.append(
-            {ldat_received[0]: element[0], 
-             ldat_received[1]: element[1], 
-             ldat_received[2]: element[2]})
+    # for element in db_out:
+    #     ret_list.append(
+    #         {ldat_received[0]: element[0], 
+    #          ldat_received[1]: element[1], 
+    #          ldat_received[2]: element[2]})
 
-    return ret_list
+    # return ret_list
 
 
 
