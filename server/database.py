7fbcd97046ad729a97408ab56ded6c04950f47f6
nico <nvidoni@ethz.ch>
2020-11-07 17:31:29 +0100

    basic get method

--- server/database.py
+++ server/database.py
@@ -1,5 +1,51 @@
-import json
+import sqlite3
+from pathlib import Path
+
+FILE = Path('.') / 'server' / 'data.db'
 
 
-def all_mock_ops():
-    return data
+def get(tbl_name, columns=None, filters=None):
+    if columns is None:
+        columns = '*'
+    else:
+        columns = ', '.join(columns)
+    sql = f'SELECT {columns} FROM {tbl_name};'
+    if filters is not None:
+        raise NotImplementedError()
+    with DatabaseCursor(FILE) as cursor:
+        cursor.execute(sql)
+        rows = cursor.fetchall()
+    return rows
+
+
+class DatabaseCursor:
+    def __init__(self, host):
+        self.connection = None
+        self.cursor = None
+        self.host = host
+
+    def __enter__(self):
+        self.connection = sqlite3.connect(self.host)
+        self.cursor = self.connection.cursor()
+        return self.cursor
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_tb or exc_val or exc_type:
+            self.connection.close()
+        else:
+            self.connection.commit()
+            self.connection.close()
+
+
+if __name__ == '__main__':
+    import json
+    mock_ops_file = Path('.') / 'server' / 'mock_ops.json'
+    with open(mock_ops_file, mode='r') as read_file:
+        mock_ops = json.load(read_file)
+    mock_ops = [(id, mock['name'], mock['lat'], mock['long']) for id, mock in enumerate(mock_ops)]
+    with DatabaseCursor(FILE) as cursor:
+        sql = ('CREATE TABLE IF NOT EXISTS mock_ops (id '
+               'INT PRIMARY KEY, name TEXT, lat REAL, long REAL);')
+        cursor.execute(sql)
+        sql = ('INSERT OR IGNORE INTO mock_ops VALUES(?, ?, ?, ?)')
+        cursor.executemany(sql, mock_ops)
