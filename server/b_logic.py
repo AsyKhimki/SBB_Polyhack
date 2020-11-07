1db44e8f98a9f839bf4fa41362010d53819716a2
nico <nvidoni@ethz.ch>
2020-11-07 20:11:38 +0100

    allines function first dirty test

--- server/b_logic.py
+++ server/b_logic.py
@@ -31,3 +31,23 @@ def all_mock_ops():
     # return ret_list
 
 #ldat_received = ['ID', 'abbreviation', 'name', 'lat', 'long', 'didok_id']
+
+def all_lines():
+    """dirty one for now"""
+    rows = db.get('lines')
+    line_dict = {row[0]: {'name': row[1], 'admin': row[2], 'ops':[]} for row in rows}
+    
+    sql = 'SELECT line_id, op_id, km, abbr, name, lat, long, didok FROM ops INNER JOIN line_plan ON ops.id = line_plan.op_id'
+
+    rows = db.join_get(sql)
+    for row in rows:
+        line_dict[row[0]]['ops'].append(
+            {
+                'abbreviation': row[3],
+                'name': row[4],
+                'lat': row[5],
+                'long': row[6],
+                'didok': row[7]
+            }
+        )
+    return line_dict
