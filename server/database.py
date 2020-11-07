8bbe7beaa84316a2901cdca3ee6a7753fc819a7f
nico <nvidoni@ethz.ch>
2020-11-07 19:36:13 +0100

    quick and dirty table insertion

--- server/database.py
+++ server/database.py
@@ -39,13 +39,58 @@ class DatabaseCursor:
 
 if __name__ == '__main__':
     import json
-    mock_ops_file = Path('.') / 'server' / 'mock_ops.json'
-    with open(mock_ops_file, mode='r') as read_file:
-        mock_ops = json.load(read_file)
-    mock_ops = [(id, mock['name'], mock['lat'], mock['long']) for id, mock in enumerate(mock_ops)]
+    linie_file = Path('.') / 'data' / 'linie-mit-betriebspunkten.json'
+    with open(linie_file, mode='r') as read_file:
+        lines_data = json.load(read_file)
+    # print(lines_data[5]['fields'])
+    lines_tbl = []
+    ops_tbl = []
+    line_plan_tbl = []
+    for id, line in enumerate(lines_data):
+        data = line['fields']
+        lines_tbl.append((data['linie'], data['linienname'], data['verwaltung']))
+        ops_tbl.append((id, data['abkurzung_bpk'], data['bezeichnung_bpk'], data['bezeichnung_offiziell'], data['geopos'][0], data['geopos'][1], data['didok']))
+        line_plan_tbl.append((id, data['linie'], id, data['abkurzung_bps'], data['km']))
+    print(lines_tbl[0])
+    print(ops_tbl[0])
+    print(line_plan_tbl[0])
+    map_ops = {}
+    maps_nums = {}
+    unique_abbr = set()
+    new_ops = []
+    for op in ops_tbl:
+        if not op[1] in unique_abbr:
+            new_ops.append(op)
+            map_ops[op[1]] = op[0]
+            unique_abbr.add(op[1])
+        else:
+            true_id = map_ops[op[1]]
+            maps_nums[op[0]] = true_id
+    print(len(new_ops))
+    print(len({op[2] for op in line_plan_tbl}))
+    new_line_plan_tbl = []
+    for plan in line_plan_tbl:
+        if maps_nums.get(plan[2]) is not None:
+            new_line_plan_tbl.append((plan[0], plan[1], maps_nums[plan[2]], plan[3], plan[4]))
+        else:
+            new_line_plan_tbl.append(plan)
+    print(len({op[2] for op in new_line_plan_tbl}))
+    ops_tbl = new_ops
+    line_plan_tbl = new_line_plan_tbl
     with DatabaseCursor(FILE) as cursor:
-        sql = ('CREATE TABLE IF NOT EXISTS mock_ops (id '
-               'INT PRIMARY KEY, name TEXT, lat REAL, long REAL);')
+        sql = ('CREATE TABLE IF NOT EXISTS ops (id '
+               'INT PRIMARY KEY, abbr TEXT, name TEXT, stop_name TEXT, lat REAL, long REAL, didok INT);')
         cursor.execute(sql)
-        sql = ('INSERT OR IGNORE INTO mock_ops VALUES(?, ?, ?, ?)')
-        cursor.executemany(sql, mock_ops)
+        sql = ('CREATE TABLE IF NOT EXISTS lines (id '
+               'INT PRIMARY KEY, name TEXT, admin TEXT);')
+        cursor.execute(sql)
+        sql = ('CREATE TABLE IF NOT EXISTS line_plan (id '
+               'INT PRIMARY KEY, line_id INT, op_id INT, op_name_line TEXT, km REAL);')
+        cursor.execute(sql)
+        sql = ('INSERT OR IGNORE INTO lines VALUES(?, ?, ?)')
+        cursor.executemany(sql, lines_tbl)
+        sql = ('INSERT OR IGNORE INTO ops VALUES(?, ?, ?, ?, ?, ?, ?)')
+        cursor.executemany(sql, ops_tbl)
+        sql = ('INSERT OR IGNORE INTO line_plan VALUES(?, ?, ?, ?, ?)')
+        cursor.executemany(sql, line_plan_tbl)
+
