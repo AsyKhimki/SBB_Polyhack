c45e96f512e41afe9943a3b1a552d262393b519a
beta-sheet <alzbetak@ethz.ch>
2020-11-07 19:59:34 +0100

    data integration

--- client/src/components/Map/MapboxMap.jsx
+++ client/src/components/Map/MapboxMap.jsx
@@ -1,9 +1,17 @@
-import "./MapboxMap.css";
+//import "./MapboxMap.css";
 import React, { useState } from "react";
-import { MapContainer, Marker, Polyline, Popup, TileLayer, Circle } from "react-leaflet";
-import { opacityById, weightById, colorById } from './stylize_elements.js'
+import {
+  MapContainer,
+  Marker,
+  Polyline,
+  Popup,
+  TileLayer,
+  Circle,
+} from "react-leaflet";
+import { opacityById, weightById, colorById } from "./stylize_elements.js";
 
-{/*var greenIcon = icon({
+{
+  /*var greenIcon = icon({
   iconUrl: 'test_icon.png',
 
   iconSize:     [38, 95], // size of the icon
@@ -11,8 +19,8 @@ import { opacityById, weightById, colorById } from './stylize_elements.js'
   iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
   shadowAnchor: [4, 62],  // the same for the shadow
   popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-});*/}
-
+});*/
+}
 
 /*
 const colorById = (id) => {
@@ -44,9 +52,7 @@ const opacityById= (id) =>{
 }
 */
 
-export const Map = ({ markers }) => {
-  console.log(markers[0].lat);
-
+export const Map = ({ markers, lines, origin }) => {
   const sample_line = [
     {
       from_lat: "47.372406",
@@ -61,9 +67,8 @@ export const Map = ({ markers }) => {
       id: "2",
       to_lat: "47.539976",
       to_long: "7.579924",
-    } 
-  ]
-
+    },
+  ];
 
   const test_city = {
     id: "GE",
@@ -74,82 +79,90 @@ export const Map = ({ markers }) => {
     details: "What a pity",
   };
 
-  const handleClick = event => {
-    const { lat, lng } = event.latlng
-    console.log(`Clicked at ${lat}, ${lng}`)
-  }
-  
+  const handleClick = (event) => {
+    const { lat, lng } = event.latlng;
+    console.log(`Clicked at ${lat}, ${lng}`);
+  };
+
   return (
     <div>
-      <MapContainer
-        center={[markers[0].lat, markers[0].long]}
-        zoom={7}
-        scrollWheelZoom={true}
-      >
+      <MapContainer center={origin} zoom={7} scrollWheelZoom={true}>
         <TileLayer
           attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="https://www.mapbox.com/">Mapbox</a>'
           url="https://api.mapbox.com/styles/v1/beta-sheet/ckh6dba8002u21aob78yl7tep/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiYmV0YS1zaGVldCIsImEiOiJja2g2ZG5hN3QwYzlyMnJxY3hrYmtybHZqIn0.SEa-JVt3EsXaPGgx-4mnYA"
         />
 
-        <div> {markers.map(marker => {
-          console.log("printing")
-          return <Marker position={[marker.lat, marker.long]} className="my_marker">
-            <Popup>
-              Operation point {marker.name} <br /> Tell me more.
-            </Popup>
-          </Marker>
-        }) } </div>
+        <div>
+          {" "}
+          {markers.map((marker) => {
+            console.log("printing");
+            return (
+              <Marker
+                position={[marker.lat, marker.long]}
+                className="my_marker"
+              >
+                <Popup>
+                  Operation point {marker.name} <br /> Tell me more.
+                </Popup>
+              </Marker>
+            );
+          })}{" "}
+        </div>
 
-          <Marker position={[47.372406, 8.537606]}>
-            <Popup>
-              Welcome to Zurich. <br /> Easily customizable.
-            </Popup>
-          </Marker>
+        <Marker position={[47.372406, 8.537606]}>
+          <Popup>
+            Welcome to Zurich. <br /> Easily customizable.
+          </Popup>
+        </Marker>
 
         {/* TRY out polylines 
         see properties
         https://leafletjs.com/reference-1.7.1.html#path
         */}
 
-          {sample_line.map(({id, from_lat, from_long, to_lat, to_long}) => {
-            return <Polyline key={id} 
-
-              positions={[ [ from_lat, from_long], [to_lat, to_long]]} 
-              color={colorById (id)}
-              stroke = {true}
+        {sample_line.map(({ id, from_lat, from_long, to_lat, to_long }) => {
+          return (
+            <Polyline
+              key={id}
+              positions={lines}
+              color={colorById(id)}
+              stroke={true}
               opacity={opacityById(id)}
               bubblingMouseEvents={false}
-              weight={weightById(id)} />
-
-          })}
+              weight={weightById(id)}
+            />
+          );
+        })}
 
         {/* Add new type of a marker
-        */}
+         */}
 
-              <Marker 
-                key={test_city.id} 
-                position={{lat:46.236360, lng: 6.139854}}
-                onClick={handleClick}>
-                <Popup>
-                  <span>
-                    {test_city.full_name}<br />
-                    {test_city.status}<br />
-                    {test_city.details}<br />
-                    
-                    {/*
+        <Marker
+          key={test_city.id}
+          position={{ lat: 46.23636, lng: 6.139854 }}
+          onClick={handleClick}
+        >
+          <Popup>
+            <span>
+              {test_city.full_name}
+              <br />
+              {test_city.status}
+              <br />
+              {test_city.details}
+              <br />
+
+              {/*
                     {elem.vehicles_launched.map((elem, i) => {
                       return ( <p key={i}>{elem}</p>)
                     })} */}
-                  </span>
-                </Popup>
-                <Circle 
-                  center={{lat:46.236360, lng: 6.139854}}
-                  fillColor="red" 
-                  radius={1000}/>
-              </Marker>
-
-       
-
+            </span>
+          </Popup>
+          <Circle
+            center={{ lat: 46.23636, lng: 6.139854 }}
+            fillColor="red"
+            radius={1000}
+          />
+        </Marker>
       </MapContainer>
     </div>
   );
--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -13,18 +13,32 @@ import ReactDOM from "react-dom";
 
 
 const App = () => {
-  const [markers, setMarkers] = useState([{
-    lat: 47.372406,
-    long: 8.537606
-  }]);
-  
+  const [origin, setOrigin] = useState([47.372406, 8.537606 ]);
+  const [markers, setMarkers] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [lines, setLines] = useState([]);
+
+// for now we're fetching the same data twice - the idea is to fetch markers 
+// from one route and the lines from another
+  const fetchLines = async() => {
+    try {
+      const response = await fetch("/allops");
+      const data = await response.json();
+      console.log("Fetching lines successful!");
+      const dataArr = data.map(el => [el.lat, el.long]);
+      console.log(dataArr);
+      setLines(dataArr);
+    } catch (err) {
+      console.log("There was a problem with backend connection");
+      return;
+    }
+  }
 
   const fetchMarkers = async() => {
     try {
       const response = await fetch("/allops");
       const data = await response.json();
-      console.log("Fetch successful!");
+      console.log("Fetching markers successful!");
       console.log(data);
       setMarkers(data);
     } catch (err) {
@@ -35,7 +49,8 @@ const App = () => {
 
   const fetchData = async() => {
     setLoading(true);
-    await fetchMarkers();
+    //await fetchMarkers();
+    await fetchLines();
     setLoading(false)
   }
 
@@ -53,7 +68,7 @@ const App = () => {
         </td>
         <td className="map-container">
         <Searchbar fetchMarkers={fetchData}/>
-        <Map markers={markers}/>
+        <Map markers={markers} lines={lines} origin={origin}/>
         </td>
         <td>
           <div className="info-container">
