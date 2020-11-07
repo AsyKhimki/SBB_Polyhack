bf2679364f9b4be1c40964f17d900087ba7a9d41
Maxim Polikarpov <38730201+maximka48@users.noreply.github.com>
2020-11-07 17:19:51 +0100

    Update b_logic.py
    added level with conversion into proper list of dictionaries after getting back from the SQL

--- server/b_logic.py
+++ server/b_logic.py
@@ -1,9 +1,37 @@
 import database as db
 
 
+
 def all_mock_ops():
+    
     """
     starting point for you
-    call the db.get(mock_ops, [lsit of columns (specify from abbreviation, name, lat, long, didok_id)], optional=Filters"""
-    # process the data into dictionary format and return
-    return data
+    call the db.get(mock_ops, [lsit of columns (specify from ID, abbreviation, name, lat, long, didok_id)], optional=Filters
+    
+    Basic function for now                           
+    """
+    
+    #list of data that we receive from front end
+    #ldat_received = ['ID', 'abbreviation', 'name', 'lat', 'long', 'didok_id']
+    ldat_received = ['name', 'lat', 'long']
+    
+    
+    # get the data
+    #db_out = (db.get(mock_ops, ldat_received, filters = None))
+    db_out = (db.get(mock_ops, ldat_received))
+    
+    
+    # convert the data 
+    ret_list = []
+    
+    for element in db_out:
+        ret_list.append(
+            {ldat_received[0]: element[0], 
+             ldat_received[1]: element[1], 
+             ldat_received[2]: element[2]})
+
+    return ret_list
+
+
+
+
